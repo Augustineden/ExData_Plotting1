8054288e599fa7e016ed02d625b755925ef38a3a
Augustineden <augustineden@gmail.com>
2020-07-24 16:06:09 +0100

    Plot3 code final

--- Plot3.R
+++ Plot3.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(lubridate)
 
 data <- read.table("household_power_consumption.txt", sep = ";", header=TRUE)
 dataF <- filter(data, Date == "1/2/2007" | Date == "2/2/2007")
@@ -6,9 +7,11 @@ data <- dataF %>% unite(Date, c("Date", "Time"))
 data$Date <- dmy_hms(data$Date)
 
 png(filename="Plot3.png")
-with(data, plot(Date, Sub_metering_1, type="l", xlab="", ylab="Energy sub metering"))
-with(data, lines(Date, Sub_metering_2, col = "red"))
-with(data, lines(Date, Sub_metering_3, col = "blue"))
-legend("topright", lty=1, col = c("black", "red", "blue"), 
-       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+with(data, {
+        plot(Date, Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
+        lines(Date, Sub_metering_2, col = "red")
+        lines(Date, Sub_metering_3, col = "blue")
+        legend("topright", lty=1, col = c("black", "red", "blue"), 
+                legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+})
 dev.off()
