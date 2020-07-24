4ec420ed359435c4657ba1ac914777a35c87c9ce
Augustineden <augustineden@gmail.com>
2020-07-24 16:11:17 +0100

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(lubridate)
 
 data <- read.table("household_power_consumption.txt", sep = ";", header=TRUE)
 dataF <- filter(data, Date == "1/2/2007" | Date == "2/2/2007")
